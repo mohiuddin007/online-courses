bd6a7a82980056bd7cdc6324c254ffaa95a16a24
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-08-27 02:41:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import fakeData from './fakeData/fakeData';
+import Courses from './components/Courses/Courses';
+
 
 function App() {
+     const courseDetail = fakeData;
+    //  console.log(courseDetail);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container-fluid px-2">
+      <h1 className="text-center">Online Educational Courses</h1>
+      <div className="row">
+          <div className="col-md-3">
+
+          </div>
+          <div className="col-md-9 row border-left">
+            {courseDetail.map(course => <Courses course={course}/>)}
+             
+          </div>
+      </div>
+     </div>
   );
 }
 
---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Courses.css'
+
+const Courses = (props) => {
+    const {img, title, instructor, rating, price} = props.course;
+    console.log(props)
+    return (
+        <div className="col-md-4">
+            <div className="single-item text-center my-4">
+                <div className="card p-2">
+                  <img className="img-fluid img-thumbnail" id="image" src={img} alt=""/>
+                    <div className="card-body">
+                        <h4>{title}</h4>
+                        <p><small>instructor: {instructor}</small></p>
+                        <p>Rating: {rating}</p>
+                        <h5>Price: {price}</h5>
+                        <button className="btn btn-sm btn-primary">Enroll Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};   
+export default Courses;
