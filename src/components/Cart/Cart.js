7bb0f228ae33b95deb9302321f0baf64ee3b51e4
webdevelopermohiuddin <mohiuddinmazumder94@gmail.com>
2020-08-27 17:14:48 +0600

    assignment complete

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Cart = (props) => {
     const {title, price} = props.courseInfo;
-    console.log(props)
+    
     return (
         <div>
             <li className="p-3 mb-2 bg-dark text-white">
